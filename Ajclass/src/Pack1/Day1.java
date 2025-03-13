a7050219b549c494e2915de433ebc84a5cc9cac4
ajmal <ajmal@Ajimal>
2025-03-13 11:43:44 +0530

    New

--- Ajclass/src/Pack1/Day1.java
+++ Ajclass/src/Pack1/Day1.java
@@ -4,7 +4,7 @@ public class Day1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Coders");
+		System.out.println("Hello Java Coders");
 	}
 
 }
